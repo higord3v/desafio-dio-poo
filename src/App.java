b3ffa7f356bc24cfa1faa024e2a119afc528e704
higord3v <hgrprofissional@gmail.com>
2022-08-05 20:54:34 -0300

    mentoria e curso

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso = new Curso();
+        Curso curso2 = new Curso();
+        
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("POO Java");
+        curso.setCargaHoraria(4);
+
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("POO JavaScript");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println("curso:" + curso);
     }
 }
